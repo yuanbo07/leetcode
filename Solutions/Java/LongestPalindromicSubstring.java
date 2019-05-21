b5b7fcdaa9f8858679ed69e2224511e9e4aa06ea
Yuanbo WANG <yuanbo.wang07@gmail.com>
2019-05-21 22:10:25 +0200

    Test 5 using dynamic programming

--- Solutions/Java/LongestPalindromicSubstring.java
+++ Solutions/Java/LongestPalindromicSubstring.java
@@ -5,40 +5,38 @@ public class Main {
     public static void main(String[] args) {
 
         Solution solution = new Solution();
-        String longestPalindrome = solution.longestPalindrome("babad");
+        String longestPalindrome = solution.longestPalindrome("a");
         System.out.println(longestPalindrome);
     }
 }
 
 class Solution {
     public String longestPalindrome(String s) {
+        if (null == s || s.length() == 0) {
+            return s;
+        }
         int len = s.length();
         boolean[][] dp = new boolean[len][len];
-        int longestPalindromeLen = 0;
-        int longestPalindromeStartPos = 0;
+        int maxLen = 0;
+        String longestPalindrome = "";
 
-        // i stands for the length of the current string
-        for (int i = 0; i < len; i++) {
-            // j stands for the starting position of the current string
-            for (int j = 0; j < len - i; j++) {
-                // if the string size if 0 or 1, it must be a palindrome
-                if (i <= 2) {
-                    dp[j][j + i] = true;
-                    // if the string size is larger than 1, if the two extrémités of the string are equal
-                } else if (s.charAt(j + i) == s.charAt(j)) {
-                    // then if its inner string is a palindrome, it's also a palindrome
-                    dp[j][j + i] = dp[j + 1][j + i - 1];
-                } else {
-                    dp[j][j + i] = false;
-                }
-                // if the string is a palindrome whose length is bigger, we take it
-                if (dp[j][j + i] && i > longestPalindromeLen) {
-                    longestPalindromeLen = i;
-                    longestPalindromeStartPos = j;
+        // j stands for the ending position of the current string
+        for (int j = 0; j < len; j++) {
+            // i stands for the starting position of the current string
+            for (int i = 0; i <= j; i++) {
+                // checks:
+                // 1. two extremity characters are equal
+                // 2. if the length is smaller than 2 && 1 => palindrome (e.g. "a" or "aa")
+                // 3. inner string is already a palindrome => dynamic programming
+                dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i <= 2 || dp[i + 1][j - 1]);
+                // update the max length
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    // note that right side of substring is exclusive
+                    longestPalindrome = s.substring(i, j + 1);
                 }
             }
         }
-        // note that right side of substring is exclusive
-        return s.substring(longestPalindromeStartPos, longestPalindromeStartPos + longestPalindromeLen + 1);
+        return longestPalindrome;
     }
 }
