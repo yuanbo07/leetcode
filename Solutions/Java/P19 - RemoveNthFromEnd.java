13b1850eaec333627177dae418fc24928bc20a23
Yuanbo <yuanbo07@users.noreply.github.com>
2019-07-02 23:59:14 +0200

    P19 : Remove Nth From End

---
+++ Solutions/Java/P19 - RemoveNthFromEnd.java
@@ -0,0 +1,44 @@
+package main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ListNode ln1 = new ListNode(1);
+        ListNode ln2 = new ListNode(2);
+        ListNode ln3 = new ListNode(3);
+        ListNode ln4 = new ListNode(4);
+        ListNode ln5 = new ListNode(5);
+        ln1.next = ln2;
+        ln2.next = ln3;
+        ln3.next = ln4;
+        ln4.next = ln5;
+        System.out.println(new Solution().removeNthFromEnd(ln1, 2));
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+class Solution {
+    // two passes
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        int length  = 0;
+        ListNode first = head;
+        while (first != null) {
+            length++;
+            first = first.next;
+        }
+        length -= n;
+        first = dummy;
+        while (length > 0) {
+            length--;
+            first = first.next;
+        }
+        first.next = first.next.next;
+        return dummy.next;
+    }
+}
